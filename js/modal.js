b6399b421a611b0a27ec8c4cfc66157839d08127
Juan Restrepo <9juanrivera@gmail.com>
2026-03-30 22:10:57 -0500

    Actualización de las modals y nombres

--- js/modal.js
+++ js/modal.js
@@ -2,23 +2,46 @@ const imagenes = document.querySelectorAll(".abrir");
 const modal = document.getElementById("modal");
 const titulo = document.getElementById("titulo");
 const texto = document.getElementById("texto");
+const galeria = document.getElementById("galeria");
+
 const cerrar = document.querySelector(".cerrar");
 
-// abrir modal
 imagenes.forEach(img => {
   img.addEventListener("click", () => {
-    modal.style.display = "flex"; // muestra el modal
     titulo.textContent = img.dataset.titulo;
     texto.textContent = img.dataset.texto;
+
+    // el limpiado de las fotos de los otros
+    galeria.innerHTML = "";
+
+    // agarrar la imagen
+    const imagenesArray = (img.dataset.imagen || img.src).split(",");
+
+    imagenesArray.forEach((src, index) => {
+      const ruta = src.trim();
+
+      const nuevaImg = document.createElement("img");
+      nuevaImg.src = ruta;
+
+
+      // click en miniatura
+      nuevaImg.addEventListener("click", () => {
+        imagenPrincipal.src = ruta;
+      });
+
+      galeria.appendChild(nuevaImg);
+    });
+
+    modal.style.display = "flex";
   });
 });
 
-// cerrar modal
+// cerrar con la X
 cerrar.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-// cerrar si se hace click afuera
+// cerrar haciendo click afuera
 window.addEventListener("click", (e) => {
   if (e.target === modal) {
     modal.style.display = "none";
